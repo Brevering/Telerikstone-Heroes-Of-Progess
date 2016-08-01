d982c08b1370504ceba6eb7ec22cc4702a363814
belchev.gb@gmail.com <belchev.gb@gmail.com>
2016-08-01 11:19:40 +0300

    Cuki's deck

--- js/cards/decks.js
+++ js/cards/decks.js
@@ -1,19 +1,7 @@
 (function () {
     'use strict';
 
-    define(['minionCard'], function (MinionCard) {
-        function getRandomValue() {
-            return [1, 2, 3, 4, 5, 6, 7, 8, 9][Math.floor(Math.random() * 3)];
-        }
-
-        function getRandomStats() {
-            let attack = getRandomValue();
-            let mana = getRandomValue();
-            let health = getRandomValue();
-
-            return [attack, mana, health];
-        }        
-
+    define(['minionCard', 'powerCard', 'weaponCard'], function (MinionCard, PowerCard, WeaponCard) {
         let standartDeck = [
             new MinionCard(getRandomStats()[0], getRandomStats()[1], getRandomStats()[2], 'images/cards/Team-Crazy-Cat/atacata.png', 'minion', function () { }),
             new MinionCard(getRandomStats()[0], getRandomStats()[1], getRandomStats()[2], 'images/cards/Team-Crazy-Cat/catinbath.png', 'minion', function () { }),
@@ -29,12 +17,47 @@
             new MinionCard(getRandomStats()[0], getRandomStats()[1], getRandomStats()[2], 'images/cards/Team-Crazy-Cat/wooki.png', 'minion', function () { })
         ];
 
+        function getRandomValue() {
+            return [1, 2, 3, 4, 5, 6, 7, 8, 9][Math.floor(Math.random() * 10)];
+        }
+
+        function getRandomStats() {
+            let attack = getRandomValue();
+            let mana = getRandomValue();
+            let health = getRandomValue();
+
+            return [attack, mana, health];
+        }
+
+        function getDeck(trainerDeck) {
+            let newDeck = trainerDeck.slice(0, 3);
+            let used = [];
+
+            while (newDeck.length !== 10) {
+                let standartCardIndex = Math.floor(Math.random() * 11);
+
+                if (used.indexOf(standartCardIndex) === -1) {
+                    used.push(standartCardIndex);
+                    newDeck.push(standartDeck[standartCardIndex]);
+                }
+            }
+
+            return newDeck;
+        }
+
         function Deck() {
         }
 
         Deck.prototype = {
             getCukiDeck() {
+                let cukiSpecialCards = [
+                    new MinionCard(6, 3, 9, 'images/cards/cuki-deck/avl-tree.png'),
+                    new PowerCard(9, 'images/cards/cuki-deck/dynamic-programming.png'),
+                    new WeaponCard(6, 'images/cards/cuki-deck/recursion.png'),
+                    new MinionCard(1, 3, 4, 'images/cards/cuki-deck/ninja.png')
+                ];
 
+                return getDeck(cukiSpecialCards);
             },
             getDonchoDeck() {
 
