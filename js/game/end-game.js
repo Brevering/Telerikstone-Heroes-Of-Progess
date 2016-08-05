499bf06e6f4ca7d64ae4a9601ec91da2674841ec
Alexander Yankov <alexander.yankov1173@abv.bg>
2016-08-05 21:53:44 +0300

    end game screen finished

--- js/game/end-game.js
+++ js/game/end-game.js
@@ -11,34 +11,78 @@
             if (enemyAvatar.health <= 0) {
                 console.log('GAME OVER: You win!');
 
-                activateWinSceen(playersAvatars)
+                disableCanvas(playersAvatars, true);
             } else if (playerAvatar.health <= 0) {
                 console.log('GAME OVER: You lose!');
 
-                activateWinSceen(playersAvatars)
+                disableCanvas(playersAvatars, false);
             } else if (playerCards.length <= 1) {
                 console.log('GAME OVER: You lose!');
 
-                activateWinSceen(playersAvatars)
+                disableCanvas(playersAvatars, false);
             } else if (enemyCards.length <= 1) {
                 console.log('GAME OVER: You win!');
 
-                activateWinSceen(playersAvatars)
+                disableCanvas(playersAvatars, true);
             }
         }
 
-        function activateWinSceen(playersAvatars) {
-            disableCanvas(playersAvatars);
-        }
-
-        function disableCanvas(playersAvatars) {
+        function disableCanvas(playersAvatars, isWin) {
             let canvas = document.getElementById('playFieldCanvas');
-            let animation = new TimelineMax({onComplete: enableEndGameButton.bind(null, playersAvatars)});
+            let animation = new TimelineMax({onComplete: function () {
+                createEndContainer()
+
+                if (isWin) {
+                    loadWinImage();
+                }
+                else {
+                    loadDefeatImage();
+                }
+
+                enableEndGameButton();
+            }});
 
             animation.to(canvas, 1, {width: 0, height: 0});
         }
 
+        function createEndContainer() {
+            let $endContainer = $('<div>');
+
+            $endContainer.attr('id', 'endContainer');
+
+            $endContainer.css({
+                display: 'flex',
+                'flex-flow': 'column',
+                'align-items': 'center',
+                'justify-content': 'center'
+            });
+
+            $endContainer.appendTo($('#playField'));
+        }
+
+        function loadWinImage() {
+            let $winImage = $('<img>');
+
+            $winImage.attr('src', 'images/end-images/victory.png');
+
+            $winImage.css('display', 'block');
+
+            $winImage.appendTo($('#endContainer'));
+        }
+
+        function loadDefeatImage() {
+            let $defeatImage = $('<img>');
+
+            $defeatImage.attr('src', 'images/end-images/defeat.png');
+
+            $defeatImage.css('display', 'block');
+
+            $defeatImage.appendTo($('#endContainer'));
+        }
+
         function enableEndGameButton() {
+            console.log('enabled end button');
+
             let $button = $('<a>');
 
             $button.attr('id', 'end-game');
@@ -46,10 +90,11 @@
             $button.css({
                 background: 'url(images/buttons/endGame.png)',
                 width: '256px',
-                height: '256px'
+                height: '256px',
+                display: 'block',
             });
 
-            $button.appendTo($('#playField'));
+            $button.appendTo($('#endContainer'));
         }
 
         return {
